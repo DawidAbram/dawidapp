9c589ec350a1a3c2d7bcf9a4061d5bbbd02197c9
DawidAbram <dvarm61@hotmail.com>
2024-08-07 19:59:26 +0100

    Updated home welcome title, position name and removed welcome blog post that was not needed

--- src/components/WelcomeHeader/WelcomeHeader.tsx
+++ src/components/WelcomeHeader/WelcomeHeader.tsx
@@ -1,24 +1,30 @@
 import Image from "next/image";
-import DawidAbramImage from '../../../public/dawidabram.webp';
-import styles from './WelcomeHeader.module.css';
+import DawidAbramImage from "../../../public/dawidabram.webp";
+import styles from "./WelcomeHeader.module.css";
 
 const WelcomeHeader = () => {
-    return (
-        <header className={styles.welcomeContainer}>
-            <Image
-                priority
-                src={DawidAbramImage}
-                placeholder="blur"
-                alt="Dawid Abram"
-                className={styles.image}
-            />
-            <h1>
-                Hi I’m Dawid! 👋<br/>
-                A full stack website developer specialising in javascript applications.<br/>
-                Currently working @ <a href="https://th-ey.com" target="_blank" rel="noopener noreferrer">TH-EY</a>
-            </h1>
-        </header>
-    );
-}
+  return (
+    <header className={styles.welcomeContainer}>
+      <Image
+        priority
+        src={DawidAbramImage}
+        placeholder="blur"
+        alt="Dawid Abram"
+        className={styles.image}
+      />
+      <div>
+        <h1>Hi I’m Dawid Abram! 👋</h1>
+        <p>
+          A lead developer specialising in javascript applications.
+          <br />
+          Currently working at{" "}
+          <a href="https://th-ey.com" target="_blank" rel="noopener noreferrer">
+            TH-EY
+          </a>
+        </p>
+      </div>
+    </header>
+  );
+};
 
 export default WelcomeHeader;
