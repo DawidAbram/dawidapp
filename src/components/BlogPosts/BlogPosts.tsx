d1d5c72b1d0b6d4fa4f72d0097cd24e35b554adf
DawidAbram <dvarm61@hotmail.com>
2024-08-07 23:21:21 +0100

    Simplifying home page

--- src/components/BlogPosts/BlogPosts.tsx
+++ src/components/BlogPosts/BlogPosts.tsx
@@ -1,10 +1,10 @@
 import styles from "@/components/BlogPosts/BlogPosts.module.css";
 import Link from "next/link";
-import {BlogPost} from "@/utils/helpers";
+import { BlogPost } from "@/utils/helpers";
 import Image from "next/image";
 
 const BlogPosts = ({ posts }: { posts: BlogPost[] }) => {
-    const convertImage = (w: number, h: number) => `
+  const convertImage = (w: number, h: number) => `
         <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
             <defs>
                 <linearGradient id="g">
@@ -19,42 +19,28 @@ const BlogPosts = ({ posts }: { posts: BlogPost[] }) => {
         </svg>
     `;
 
-    const toBase64 = (str: string) =>
-        typeof window === 'undefined'
-            ? Buffer.from(str).toString('base64')
-            : window.btoa(str);
+  const toBase64 = (str: string) =>
+    typeof window === "undefined"
+      ? Buffer.from(str).toString("base64")
+      : window.btoa(str);
 
-    const getShortTitle = (title: string, maxAmount = 55) => {
-        if (title.length > maxAmount) {
-            return title.substring(0, maxAmount) + '...';
-        }
-        return title;
-    };
-
-    return (
-        <div className={styles.blogPostsGrid}>
-            {posts.map(post => (
-                <Link href={`/blog/${post.slug}`} className={styles.blogPost} key={post.slug}>
-                    <Image
-                        src={post.data.imageUrl}
-                        alt="Blog post title"
-                        placeholder="blur"
-                        blurDataURL={`data:image/svg+xml;base64,${toBase64(
-                            convertImage(320, 190)
-                        )}`}
-                        width={320}
-                        height={190}
-                        className={styles.blogPostImage}
-                    />
-                    <div className={styles.blogPostInfo}>
-                        <p>{post.data.publishedAt}</p>
-                        <p>{post.readingTime} min. read</p>
-                    </div>
-                    <h3>{getShortTitle(post.data.title)}</h3>
-                </Link>
-            ))}
-        </div>
-    );
+  return (
+    <div className={styles.blogPostsGrid}>
+      {posts.map((post) => (
+        <Link
+          href={`/blog/${post.slug}`}
+          className={styles.blogPost}
+          key={post.slug}
+        >
+          <div className={styles.blogPostInfo}>
+            <p>{post.data.publishedAt}</p>
+            <p>{post.readingTime} min. read</p>
+          </div>
+          <h3>{post.data.title}</h3>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default BlogPosts;
